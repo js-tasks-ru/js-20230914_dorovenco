557536f3182c12b963a7e861d32cc0e4f114080d
DaniilDorovenko <dorovenco@mail.ru>
2023-09-21 14:08:05 +0300

    uniq task added

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,24 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
+// export function createGetter(path) {
+//     getter(path) { 
+//     	return this.path 
+//     }
+// }
 
+
+const product = {
+  category: {
+    title: "Goods"
+  }
 }
+
+function createGetter(path) {
+
+    	return this.path 
+    
+}
+
+const getter = createGetter('category.title');
+console.log(getter(product)); // Goods
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,7 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+	let set = new Set(arr);
+	let uniq = Array.from(set);
+	return uniq 
 }
