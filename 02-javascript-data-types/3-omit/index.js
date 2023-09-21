6e28e311d308c792f4a2c499f731352f0c6469c8
DaniilDorovenko <dorovenco@mail.ru>
2023-09-21 09:01:56 +0300

    omit func added

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -4,6 +4,14 @@
  * @param {...string} fields - the properties paths to omit
  * @returns {object} - returns the new object
  */
-export const omit = (obj, ...fields) => {
 
+export const omit = (obj, ...fields) => {
+	
+        for (let key in obj) {
+            for (let field of [...fields]) {	 
+               if (field === key) { delete obj[key];} 
+               
+        } 
+    }
+    return obj 
 };
