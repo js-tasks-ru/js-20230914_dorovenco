fe73e5bcf127142932de5307d4ac31cf63de70ed
Daniil Dorovenko <dorovenco@mail.ru>
2023-09-22 14:13:21 +0300

    some changes

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,16 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+function asc (a, b) {
+   return a.localeCompare(b, ['ru', 'en'], { caseFirst: 'upper' })
+}
+
+ function desc (a, b) {
+   return  b.localeCompare(a, ['ru', 'en'], { caseFirst: 'upper' })
+}
+
+export function sortStrings(arr, param = 'asc') {	
+	const sorted = arr. slice();
+    return param == 'asc' ? sorted.sort(asc): sorted.sort(desc);
 }
---
+++ 02-javascript-data-types/3-omit/index.js
@@ -0,0 +1,17 @@
+/**
+ * omit - creates an object composed of enumerable property fields
+ * @param {object} obj - the source object
+ * @param {...string} fields - the properties paths to omit
+ * @returns {object} - returns the new object
+ */
+
+export const omit = (obj, ...fields) => {
+	
+        for (let key in obj) {
+            for (let field of [...fields]) {	 
+               if (field === key) { delete obj[key];} 
+               
+        } 
+    }
+    return obj 
+};
