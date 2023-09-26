6db134c638a3cfeaf996dfddb1edfb19bcffba28
DaniilDorovenko <dorovenco@mail.ru>
2023-09-26 16:45:37 +0300

    skeleton added

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -19,19 +19,23 @@ export default class ColumnChart {
 
     template() {
 		const maxValue = Math.max(...Object.values(this.data));
+		console.log(this.data)
 		return (
    			`
-			    <div class="column-chart" style="--chart-height: 50">
+			    <div class="column-chart ${ (this.data.length == 0) ? 'column-chart_loading': '' }" style="--chart-height: 50">
 			        <div class="column-chart__title">
-			            Total ${ this.label }
-			            <a href="${ this.link }" class="column-chart__link">View all</a>
+			            Total ${this.label}
+			            <a href="${this.link}" class="column-chart__link">View all</a>
 			        </div>
 			        <div class="column-chart__container">
-			            <div data-element="header" class="column-chart__header">${ this.formatHeading ? this.formatHeading(this.value.toLocaleString("en-US")) : this.value }</div>
+			            <div data-element="header" class="column-chart__header">
+			            	${ this.formatHeading ? this.formatHeading(this.value.toLocaleString("en-US")) : this.value }
+			            </div>
 			            <div data-element="body" class="column-chart__chart">
 
-				            		${ this.data.map((value) => `<div style="--value: ${ value }" data-tooltip="${ Math.round((value / maxValue) * 100) }%"></div>` ).join('') }
- 
+							${this.data.map((value) => `
+								<div style="--value: ${value}" data-tooltip="${Math.round((value / maxValue) * 100)}%"></div>
+								` ).join('')}
 			            </div>
 			        </div>
 			    </div>
