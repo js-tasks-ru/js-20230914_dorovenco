bd02eb3c36b5079ed450f384b7c813463796a0b9
Daniil Dorovenko <dorovenco@mail.ru>
2023-10-04 15:34:41 +0300

    notification task tests passed

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,57 @@
 export default class NotificationMessage {
 
+element;
+
+constructor(message, props = {}) {
+	const {
+		duration = 20000,
+		type = 'success',
+	} = props;
+
+	this.duration = duration;
+	this.type = type;
+	this.message = message;
+
+	this.element = this.createElement();
+
+}
+
+  createElement() {
+    const element = document.createElement('div');
+    element.innerHTML = this.createTemplate();
+    return element.firstElementChild
+  }
+
+remove() {
+    this.element.remove();
+
+  }
+
+  destroy() {
+    this.remove();
+  }
+
+  show(targetElement = document.body) {
+    targetElement.append(this.element)
+
+    setTimeout(() => {
+      this.destroy();
+    }, this.duration);
+  }
+
+
+ createTemplate() { 
+  		return (
+  		`
+  		<div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">success</div>
+      <div class="notification-body">
+        ${this.message}
+      </div>
+    </div>
+  </div>
+  		`  ) 
+  }
 }
