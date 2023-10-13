35edb7cefe75d9cfdeb216a390b6734955aa8635
DaniilDorovenko <dorovenco@mail.ru>
2023-10-13 08:36:31 +0300

    column chart with fetched data task added

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -1,7 +1,34 @@
 import fetchJson from './utils/fetch-json.js';
 
-const BACKEND_URL = 'https://course-js.javascript.ru';
+import { ColumnChart as ColumnChartV1 } from "../../04-oop-basic-intro-to-dom/1-column-chart/index.js";
 
-export default class ColumnChart {
+export default class ColumnChart extends ColumnChartV1 {
 
+  API = 'https://course-js.javascript.ru/'
+
+  constructor(props = {}) {
+
+    super(props);
+
+    this.url = new URL(this.API + props.url);
+    this.range = props.range;
+
+  }
+
+  async update(from = this.range.from, to = this.range.to) {
+
+    this.url.searchParams.set('from', from.toISOString());
+    this.url.searchParams.set('to', to.toISOString());
+
+    const response = await fetchJson(this.url);
+
+    this.data = Object.values(response);
+
+    this.subElements.body.innerHTML = this.createColumnsPropsTemplate();
+
+    this.element.classList.remove('column-chart_loading');
+
+    return response;
+
+  }
 }
